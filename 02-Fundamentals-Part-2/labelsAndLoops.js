7dc8c87e5b27c888f5ebc6318fabde7d9e0c9b71
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-09-21 14:16:40 +0100

    Update labels and loops
    - messing around with more fancy looping methods

--- 02-Fundamentals-Part-2/labelsAndLoops.js
+++ 02-Fundamentals-Part-2/labelsAndLoops.js
@@ -1,4 +1,9 @@
-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#for_statement
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#for_statement
+'use strict';
+
+String.prototype.replaceAt = function (i, x) {
+    return this.substring(0, i) + x + this.substring(i + x.length);
+}
 
 // shadowing
 for (let i = 0; i < 5; i++) {
@@ -11,6 +16,31 @@ for (let i = 0; i < 5; i++) {
 
 nl();
 
+// looping through arrays
+let a = Array(10).fill(0);
+let s = "[";
+for (let x of a) s += `${x},`;
+s = s.replaceAt(s.length-1, "]");
+console.log(s);
+
+nl();
+
+// looping through objects
+const obj = {x: "1", y: "2", z: "3", f: function() { console.log(); }};
+for (const pr in obj) {
+    console.log(`obj.${pr} = ${obj[pr]}`);
+}
+
+nl();
+
+for (const [key, val] of Object.entries(obj)) {
+    console.log(`obj.${key} = ${val}`);
+}
+
+;;;
+
+nl();
+
 // do while
 do {
     console.log("block is executed");
@@ -18,7 +48,7 @@ do {
 
 // do..while: one line of code
 do
-    console.log("block is executed");
+console.log("block is executed");
 while (false)
 
 nl();
