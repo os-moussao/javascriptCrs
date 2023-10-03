47d591176d22dacb19ab867b520d51df0c2f12bd
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-10-03 14:35:37 +0100

    Update promise.js
    - throwing errors manually, in case fetch didn't

--- 16-Asynchronous/promise.js
+++ 16-Asynchronous/promise.js
@@ -5,10 +5,12 @@ a promise is a placeholder for a future result of an asynchronous operation
 */
 
 logCountryData("morocco");
+logCountryData("foo");
 
 function logCountryData(country) {
     const ret = fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then((resp) => {
+        if (!resp.ok) throw new Error(`Country not found (${resp.status})`);
         return resp.json(); // this is also a promise
     })
     .then((data) => console.log(data[0]))
