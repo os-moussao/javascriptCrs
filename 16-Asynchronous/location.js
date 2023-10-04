59be9f9a2f9230af4e60a5a0c34a7e9385d008fb
Oussama Moussaoui <omoussaoui1337@gmail.com>
2023-10-04 11:56:40 +0100

    Add location.js
    - getting data from geocode api

---
+++ 16-Asynchronous/location.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const $1337 = { lat: 32.882162410413294, lng: -6.897813811155632 }
+
+logGeoData($1337);
+
+
+
+
+async function logGeoData(pos) {
+    try {
+        let data = await fetch(makeUrl($1337));
+    
+        if (!data.ok) throw new Error(`cannot fetch data (${data.status})!`);
+
+        data = await data.json();
+
+        if (!data.country) throw Error(`server exhausted, try later!`);
+        
+        console.log("You're in: ");
+        console.log(`- Country: ${data.country}`);
+        console.log(`- State: ${data.state}`);
+        console.log(`- City: ${data.city}`);
+        console.log(`- Street: ${data.staddress} ${data.stnumber}`);
+
+    } catch (err) {
+        console.error("Error:", err.message);
+    }
+}
+
+function makeUrl({lat, lng}) {
+    return `http://geocode.xyz/${lat},${lng}-6.897794140374616?geoit=json`
+}
